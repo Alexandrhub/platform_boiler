e73947c1f849e2d871d260a112c55192cc2c223c
Alexandr <alexandrhub@vk.com>
2024-01-16 15:45:04 +0300

    add platform common

---
+++ pkg/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"github.com/joho/godotenv"
+)
+
+func Load(path string) error {
+	err := godotenv.Load(path)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type GRPCConfig interface {
+	Address() string
+}
+
+type PGConfig interface {
+	DSN() string
+}
+
+type HTTPConfig interface {
+	Address() string
+}
+
+type SwaggerConfig interface {
+	Address() string
+}
+
+type PrometheusConfig interface {
+	Address() string
+}
